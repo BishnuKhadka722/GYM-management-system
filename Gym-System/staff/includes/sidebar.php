61c8fe43c5726c378298c4d25a6770c64c306e5a
BishnuKhadka722 <119380563+BishnuKhadka722@users.noreply.github.com>
2025-01-28 08:39:18 +0545

    Further work on staff page

---
+++ Gym-System/staff/includes/sidebar.php
@@ -0,0 +1,13 @@
+<!--sidebar-menu-->
+<div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
+  <ul>
+    <li class="<?php if($page=='dashboard'){ echo 'active'; }?>"><a href="index.php"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+   
+    </li>
+    <li class="<?php if($page=='membersts'){ echo 'active'; }?>"><a href="member-status.php"><i class="icon icon-eye-open"></i> <span>Member's Status</span></a></li>
+    <li class="<?php if($page=='payment'){ echo 'active'; }?>"><a href="payment.php"><i class="icon icon-money"></i> <span>Payments</span></a></li>
+    <li class="<?php if($page=='attendance'){ echo 'active'; }?>"><a href="attendance.php"><i class="icon icon-calendar"></i> <span>Manage Attendance</span></a></li>
+
+  </ul>
+</div>
+<!--sidebar-menu-->
