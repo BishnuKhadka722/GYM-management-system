552fef45e224827c8b14548096e1d0f60f9ac7af
BishnuKhadka722 <119380563+BishnuKhadka722@users.noreply.github.com>
2025-03-11 13:43:46 +0545

    Update userpay.php

--- Gym-System/staff/staff-pages/userpay.php
+++ Gym-System/staff/staff-pages/userpay.php
@@ -2,7 +2,17 @@
 session_start();
 //the isset function to check username is already loged in and stored on the session
 if(!isset($_SESSION['user_id'])){
-header('location:../index.php');	
+    header('location:../index.php');	
+}
+
+// Check if coming from successful payment
+if(isset($_SESSION['payment_successful']) && isset($_SESSION['payment_data'])) {
+    // Set POST data from session for compatibility with existing code
+    $_POST = $_SESSION['payment_data'];
+    
+    // Clear session data to prevent duplicate processing
+    unset($_SESSION['payment_successful']);
+    unset($_SESSION['payment_data']);
 }
 ?>
 
@@ -64,7 +74,7 @@ header('location:../index.php');
 
             $amountpayable = $amount * $plan;
             
-            include '../../dbcon.php';
+            include 'dbcon.php';
             date_default_timezone_set('Asia/Kathmandu');
             //$current_date = date('Y-m-d h:i:s');
                 $current_date = date('Y-m-d h:i A');
@@ -334,51 +344,4 @@ table td {
 
 <!--end-Footer-part-->
 
-<script src="../js/excanvas.min.js"></script> 
-<script src="../js/jquery.min.js"></script> 
-<script src="../js/jquery.ui.custom.js"></script> 
-<script src="../js/bootstrap.min.js"></script> 
-<script src="../js/jquery.flot.min.js"></script> 
-<script src="../js/jquery.flot.resize.min.js"></script> 
-<script src="../js/jquery.peity.min.js"></script> 
-<script src="../js/fullcalendar.min.js"></script> 
-<script src="../js/matrix.js"></script> 
-<script src="../js/matrix.dashboard.js"></script> 
-<script src="../js/jquery.gritter.min.js"></script> 
-<script src="../js/matrix.interface.js"></script> 
-<script src="../js/matrix.chat.js"></script> 
-<script src="../js/jquery.validate.js"></script> 
-<script src="../js/matrix.form_validation.js"></script> 
-<script src="../js/jquery.wizard.js"></script> 
-<script src="../js/jquery.uniform.js"></script> 
-<script src="../js/select2.min.js"></script> 
-<script src="../js/matrix.popover.js"></script> 
-<script src="../js/jquery.dataTables.min.js"></script> 
-<script src="../js/matrix.tables.js"></script> 
-
-<script type="text/javascript">
-  // This function is called from the pop-up menus to transfer to
-  // a different page. Ignore if the value returned is a null string:
-  function goPage (newURL) {
-
-      // if url is empty, skip the menu dividers and reset the menu selection to default
-      if (newURL != "") {
-      
-          // if url is "-", it is this page -- reset the menu:
-          if (newURL == "-" ) {
-              resetMenu();            
-          } 
-          // else, send page to designated URL            
-          else {  
-            document.location.href = newURL;
-          }
-      }
-  }
-
-// resets the menu selection upon entry to this page:
-function resetMenu() {
-   document.gomenu.selector.selectedIndex = 2;
-}
-</script>
-</body>
-</html>
+<script src="../js
